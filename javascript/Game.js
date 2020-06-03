37a38886e8ce6a2d250b454f318106d11e288d02
GabrielleFreno01 <61683857+GabrielleFreno01@users.noreply.github.com>
2020-06-03 11:16:04 +0200

    Fin du jeu

--- javascript/Game.js
+++ javascript/Game.js
@@ -1,250 +1,320 @@
-var player;
+//Create canvas
+//var canvas = document.createElement("canvas");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+/*ctx.font = '30px Arial';
+canvas.width = 1500;
+canvas.height = 670;
+canvas.style = "border:1px solid #000000;";
+var div = document.getElementById("gameZone");
+div.appendChild(canvas);*/
 
-var apricotList = {};
-var cheeseList = {};
-var barrelList = {};
-var wineList = {};
-var papetList = {};
-var viesList = {};
-var ok = 0;
-var collid = false;
-var alive = true;
+//for player
+var HEIGHT = 670;
+var WIDTH = 1500;
+//for other entities
+var HEIGHT2 = 580;
+var timeWhenGameStarted = Date.now(); //return time in ms
+
+var frameCount = 0;
+var cptVie;
+var score = 0;
+var highScore = 0;
+var pause = false;
+
+var speedX = -1;
+var speedY = 0;
+
+var levelArray;
+
+var bg;
 
 
+var Img = {};
+Img.constantin = new Image();
+Img.constantin.src = "img/bus-FCSION.png";
+Img.freysinger = new Image();
+Img.freysinger.src = "img/subaru.png";
+Img.rappaz = new Image();
+Img.rappaz.src = "img/bus-HIPPIE.png";
+/*Img.enemy = new Image();
+Img.enemy.src = "img/enemy.png";*/
+Img.barrel = new Image();
+Img.barrel.src = "img/barrel.png";
+Img.cheese = new Image();
+Img.cheese.src = "img/cheese.png";
+Img.wine = new Image();
+Img.wine.src = "img/wine.png";
+Img.apricot = new Image();
+Img.apricot.src = "img/apricot.png";
+Img.papet = new Image();
+Img.papet.src = "img/papet.png";
+Img.bg = new Image();
+Img.bg.src = "img/back.png";
+Img.coeur = new Image();
+Img.coeur.src = "img/coeur.png";
+Img.flag = new Image();
+Img.flag.src = "img/flag.png";
+/*Img.bullet = new Image();
+Img.bullet.src = "img/bullet.png";*/
 
-Player = function () {
-	//Generate the hero choosed
-	var self;
-	switch (heroChoosedId) {
-		case "constantin":
-			self = Entity('player', 'id', 300, 200, 2, 2, 200, 134, Img.constantin);
-			break;
-		case "freysinger":
-			self = Entity('player', 'id', 300, 200, 2, 2, 200, 134, Img.freysinger);
-			break;
-		case "rappaz":
-			self = Entity('player', 'id', 300, 200, 2, 2, 200, 134, Img.rappaz);
-			break;
-		default:
-			self = Entity('player', 'id', 300, 200, 2, 2, 200, 134, Img.freysinger);
+testCollisionRectRect = function (rect1, rect2) {
+	return rect1.x <= rect2.x + rect2.width &&
+		rect2.x <= rect1.x + rect1.width &&
+		rect1.y <= rect2.y + rect2.height &&
+		rect2.y <= rect1.y + rect1.height;
+}
+
+function generateLevel() {
+	//Read level1.json
+	var requestURL = "level1.json";
+	var request = new XMLHttpRequest();
+	request.open('GET', requestURL);
+	request.responseType = 'json';
+	request.send();
+	var level;
+
+	request.onload = function () {
+		level = request.response;
+
+		console.log(level);
+		//Get the array of the level
+		levelArray = level.terrain;
+		readJSON();
 	}
 
-	self.updatePosition = function () {
-		if (self.pressingRight) {
-			if (self.x < 1400)
-				self.x += self.spdX;
+
+	var row = 0;
+
+	//Read the column of objects to display each 2 seconds
+	readJSON = function () {
+		for (var col = 0; col < levelArray.length; col++) {
+			var y = col * 100 + 50;
+			createEntity(levelArray[col][row], 1600, y);
 		}
-		if (self.pressingLeft) {
-			if (self.x > 100)
-				self.x = self.x - self.spdX;
+		row++;
+		if (row <= levelArray[0].length) {
+			setTimeout(function () {
+				readJSON();
+			}, 1000);
+		} else {
+			//Stop Game because we finished it
+			console.log("fini");
+
 		}
-		if (self.pressingDown) {
-			if (self.y < 600)
-				self.y += self.spdY;
-		}
-		if (self.pressingUp) {
-			if (self.y > 70)
-				self.y = self.y - self.spdY;
-		}
+
 	}
 
-	var super_update = self.update;
-	self.update = function () {
-		super_update();
-		if (self.hp <= 0 && ok === 0) {
-			ok = 1;
-			var timeSurvived = Date.now() - timeWhenGameStarted;
-			console.log("You lost! You survived for " + timeSurvived + " ms.");
-			//startNewGame();
+}
+
+function listenKeys() {
+	document.onkeydown = function (event) {
+		if (event.keyCode === 68) //d
+			player.pressingRight = true;
+		else if (event.keyCode === 83) //s
+			player.pressingDown = true;
+		else if (event.keyCode === 65) //a
+			player.pressingLeft = true;
+		else if (event.keyCode === 87) // w
+			player.pressingUp = true;
+		/* else if (event.keyCode === 32) // espace
+		     player.pressingSpace = true;*/
+	}
+
+	document.onkeyup = function (event) {
+		if (event.keyCode === 68) //d
+			player.pressingRight = false;
+		else if (event.keyCode === 83) //s
+			player.pressingDown = false;
+		else if (event.keyCode === 65) //a
+			player.pressingLeft = false;
+		else if (event.keyCode === 87) // w
+			player.pressingUp = false;
+		else if (event.keyCode === 80) // p for pause
+			pause = !pause;
+	}
+}
+
+
+
+
+update = function () {
+	frameCount++;
+
+	if (pause === true) {
+		bg.stop();
+		ctx.font = '100px Minehead DEMO';
+		ctx.fillText("Pause", 650, 300);
+		//pause le level aussi
+		return;
+	}
+	if (pause === false) {
+		moveBackground();
+	}
+
+
+
+	if ((frameCount != 0) && (frameCount % 400 === 0)) {
+		score += 10;
+	}
+
+
+	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+	ctx.font = '50px Berlin Sans FB';
+
+
+
+	for (var key in viesList) {
+		viesList[key].update();
+
+	}
+
+	for (var key in apricotList) {
+		apricotList[key].update();
+		player.testCollision(apricotList[key]);
+		if (collide) {
+			score += 20;
+			player.hp -= 1;
+			console.log("vie player " + player.hp);
+			delete viesList[player.hp + 1];
+			console.log(viesList);
+			delete apricotList[key];
+		}
+
+	}
+	for (var key in cheeseList) {
+		cheeseList[key].update();
+	}
+
+	for (var key in wineList) {
+		wineList[key].update();
+
+	}
+	for (var key in papetList) {
+		papetList[key].update();
+
+	}
+	for (var key in barrelList) {
+		barrelList[key].update();
+
+	}
+	for (var key in flagList) {
+		flagList[key].update();
+		player.testCollision(flagList[key]);
+		if (collide) {
 			document.getElementById("gameZone").style.display = "none";
-			document.getElementById("gameover").style.display = "block";
+			document.getElementById("win").style.display = "block";
 			pause = true;
-
-
-
+			
 			if (score < highScore) {
-				document.getElementById("1").innerHTML = "T'as trop fait la pichte ou quoi ? C'est pas ton plus beau score";
-				document.getElementById("2").innerHTML = "Score : " + score;
-				document.getElementById("3").innerHTML = "Best : " + highScore;
+				document.getElementById("4").innerHTML = "Score : " + score;
+				document.getElementById("5").innerHTML = "Best : " + highScore;
 			} else {
 				highScore = score;
-				document.getElementById("1").innerHTML = "Mais tcheuuu beau ca ! ";
-				document.getElementById("2").innerHTML = "Un nouveau record ";
-				document.getElementById("3").innerHTML = highScore;
+				document.getElementById("4").innerHTML = "Mais tcheuuu c'est beau ca ! ";
+				document.getElementById("5").innerHTML = "Un nouveau record ";
+				document.getElementById("6").innerHTML = highScore;
 				localStorage.setItem('highscore', highScore);
 			}
 		}
 	}
 
 
-	self.pressingDown = false;
-	self.pressingLeft = false;
-	self.pressingRight = false;
-	self.pressingSpace = false;
-	self.pressingUp = false;
-
-	return self;
-}
 
 
-//player, enemy, bullet, bonus, malus, straw
-Entity = function (type, id, x, y, spdX, spdY, width, height, img) {
-	var self = {
-		type: type,
-		x: x,
-		y: y,
-		id: id,
-		width: width,
-		height: height,
-		img: img,
-		spdX: spdX,
-		spdY: spdY,
-	};
 
-	self.update = function () {
-		self.updatePosition();
+	/*
+		for (var key in strawList) {
+			strawList[key].update();
 
-		//remove object if it is out of the canevas
-		if (self.x === -100) {
-			switch (self.type) {
-				case "barrel":
-					delete barrelList[self.id];
-					break;
-				case "apricot":
-					delete apricotList[self.id];
-					break;
-				case "wine":
-					delete wineList[self.id];
-					break;
-				case "cheese":
-					delete cheeseList[self.id];
-					break;
-				case "papet":
-					delete papetList[self.id];
-					break;
-				default:
-					break;
+			player.testCollision(strawList[key]);
+			if (collide) {
+				score += 10;
+				player.hp -= 1;
+				delete viesList[player.hp + 1];
+				delete strawList[key];
+
 			}
-		} else {
-			self.draw();
+
+
 		}
+	*/
 
+	/*
+	    for (var key in bonusList) {
+	        bonusList[key].update();
+	        var isColliding = player.testCollision(bonusList[key]);
+	        if (isColliding) {
+	            if (bonusList[key].category == 'abricot')
+	                score += 10;
+	            if (bonusList[key].category == 'wine')
+	                score += 25;
+	            if (bonusList[key].category == 'cheese')
+	                score += 50;
+	            delete bonusList[key];
+	        }
+	    }*/
+
+	/*   for (var key in malusList) {
+        malusList[key].update();
+        var isColliding = player.testCollision(malusList[key]);
+        if (isColliding) {
+            if (score - 30 < 0)
+                scrore = 0;
+            else
+                score -= 30;
+
+            delete malusList[key];
+        }
+    }
+*/
+	player.update();
+
+	ctx.fillText('Score : ' + score, 1230, 655);
+	ctx.fillText('Best score : ' + highScore, 680, 655);
+
+
+}
+//Background
+moveBackground = function () {
+	$(function () {
+		deplace = function () {
+
+			bg = $('#fond').animate({
+				left: '-=250'
+			}, 1400, 'linear', function () {
+				$('#fond').css('top', 0);
+				deplace();
+			});
+		};
+		deplace();
+	});
+}
+
+
+startNewGame = function () {
+	listenKeys();
+	player.hp = 3;
+	timeWhenGameStarted = Date.now();
+	frameCount = 0;
+	/*apricotList = {};
+	cheeseList = {};
+	barrelList = {};
+	wineList = {};
+	papetList = {};*/
+	moveBackground();
+	viesList = {};
+	generateVie();
+	generateLevel();
+	setInterval(update, 5);
+	//score = 0;
+
+	//get the highscore from localstorage
+	var scoreStr = localStorage.getItem('highscore');
+	if (scoreStr == null) {
+		highScore = 0;
+	} else {
+		highScore = parseInt(scoreStr);
 	}
-
-
-	self.draw = function () {
-		ctx.save();
-
-		var x = self.x - self.width / 2;
-		var y = self.y - self.height / 2;
-		ctx.drawImage(self.img, x, y);
-		ctx.restore();
-	}
-
-
-	self.testCollision = function (entity2) { //return if colliding (true/false)
-		if (!(entity2.x > (self.x + self.width) ||
-				entity2.x < (self.x - entity2.width) ||
-				entity2.y > (self.y + self.height) ||
-				entity2.y < (self.y - entity2.height))) {
-			collide = true;
-		} else
-			collide = false;
-	}
-
-
-	self.updatePosition = function () {
-		self.x += self.spdX;
-		self.y += self.spdY;
-	}
-
-	return self;
-
-}
-
-
-Vies = function (id, x, y, spdX, spdY, width, height) {
-	var self = Entity('vie', id, x, y, spdX, spdY, width, height, Img.coeur);
-	viesList[id] = self;
-
-}
-
-generateVie = function () {
-	//Math.random() returns a number between 0 and 1
-	var x0 = 30;
-	var x1 = 90;
-	var x2 = 150;
-	var y = 645;
-	var height = 44;
-	var width = 50;
-	var id0 = 1;
-	var id1 = 2;
-	var id2 = 3;
-	var spdX = 0;
-	var spdY = 0;
-
-	Vies(id0, x0, y, spdX, spdY, width, height);
-	Vies(id1, x1, y, spdX, spdY, width, height);
-	Vies(id2, x2, y, spdX, spdY, width, height);
-}
-
-Apricot = function (id, xDestination, yDestination) {
-	var self = Entity("apricot", id, xDestination, yDestination, speedX, 0, 100, 100, Img.apricot);
-	apricotList[id] = self;
-}
-Barrel = function (id, xDestination, yDestination) {
-	var self = Entity("barrel", id, xDestination, yDestination, speedX, 0, 100, 100, Img.barrel);
-	barrelList[id] = self;
-}
-Cheese = function (id, xDestination, yDestination) {
-	var self = Entity("cheese", id, xDestination, yDestination, speedX, 0, 100, 100, Img.cheese);
-	cheeseList[id] = self;
-}
-Wine = function (id, xDestination, yDestination) {
-	var self = Entity("wine", id, xDestination, yDestination, speedX, 0, 100, 100, Img.wine);
-	wineList[id] = self;
-}
-Papet = function (id, xDestination, yDestination) {
-	var self = Entity("papet", id, xDestination, yDestination, speedX, 0, 100, 100, Img.papet);
-	papetList[id] = self;
-}
-var barrelIdCount = 0;
-var apricotIdCount = 0;
-var cheeseIdCount = 0;
-var wineIdCount = 0;
-var papetIdCount = 0;
-
-
-createEntity = function (idObject, xDestination, yDestination) {
-	switch (idObject) {
-		case 1:
-			break;
-		case 2:
-			//Créer objet apricot avec image apricot.png
-			Apricot(apricotIdCount, xDestination, yDestination)
-			apricotIdCount++;
-			break;
-		case 3:
-			//Créer objet cheese avec image cheese.png
-			Cheese(cheeseIdCount, xDestination, yDestination)
-			cheeseIdCount++;
-			break;
-		case 4:
-			//Créer objet wine avec wine.png
-			Wine(wineIdCount, xDestination, yDestination)
-			wineIdCount++;
-			break;
-		case 5:
-			//Créer objet papet avec papet.png
-			Papet(papetIdCount, xDestination, yDestination)
-			papetIdCount++;
-			break;
-		case 6:
-			//Créer objet barrel avec barrel.png
-			Barrel(barrelIdCount, xDestination, yDestination);
-			barrelIdCount++;
-			break;
-	}
-
 
 }
