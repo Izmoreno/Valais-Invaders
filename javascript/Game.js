65cfb025aec6852b76f3aca6a1e8688eb90fa59c
GabrielleFreno01 <61683857+GabrielleFreno01@users.noreply.github.com>
2020-06-05 16:13:53 +0200

    Update Game.js

--- javascript/Game.js
+++ javascript/Game.js
@@ -11,7 +11,7 @@ var timeWhenGameStarted = Date.now(); //return time in ms
 
 var frameCount = 0;
 var cptVie;
-var score = 0;
+var score = parseInt(sessionStorage.score);
 var highScore = 0;
 var pause = false;
 
@@ -21,7 +21,6 @@ var speedY = 0;
 var levelArray;
 
 var bg;
-var counter = 10;
 
 var Img = {};
 Img.constantin = new Image();
@@ -53,82 +52,83 @@ var playerName = sessionStorage.playerName;
 
 
 testCollisionRectRect = function (rect1, rect2) {
-	return rect1.x <= rect2.x + rect2.width &&
-		rect2.x <= rect1.x + rect1.width &&
-		rect1.y <= rect2.y + rect2.height &&
-		rect2.y <= rect1.y + rect1.height;
+    return rect1.x <= rect2.x + rect2.width &&
+        rect2.x <= rect1.x + rect1.width &&
+        rect1.y <= rect2.y + rect2.height &&
+        rect2.y <= rect1.y + rect1.height;
 }
 
 function generateLevel() {
-	//Read level1.json
-	var requestURL = "level1.json";
-	var request = new XMLHttpRequest();
-	request.open('GET', requestURL);
-	request.responseType = 'json';
-	request.send();
-	var level;
+    //Read level1.json
+    var requestURL = "level1.json";
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    var level;
 
-	request.onload = function () {
-		level = request.response;
+    request.onload = function () {
+        level = request.response;
 
-		//Get the array of the level
-		levelArray = level.terrain;
-		readJSON();
-	}
+        //Get the array of the level
+        levelArray = level.terrain;
+        readJSON();
+    }
 
 
-	var row = 0;
+    var row = 0;
 
-	//Read the column of objects to display each 2 seconds
-	readJSON = function () {
-		for (var col = 0; col < levelArray.length; col++) {
-			var y = col * 100 + 50;
-			createEntity(levelArray[col][row], 1600, y);
-		}
-		row++;
-		if (row <= levelArray[0].length) {
-			setTimeout(function () {
-				readJSON();
-			}, 1000);
-		} else {
-			//Stop Game because we finished it
-			if (score > highScore) {
-				localStorage.highscore = score;
-				sessionStorage.bestScore = true;
-				sessionStorage.score = score;
-			}
-		}
+    //Read the column of objects to display each 2 seconds
+    readJSON = function () {
+        for (var col = 0; col < levelArray.length; col++) {
+            var y = col * 100 + 50;
+            createEntity(levelArray[col][row], 1600, y);
+        }
+        row++;
+        if (row <= levelArray[0].length) {
+            setTimeout(function () {
+                readJSON();
+            }, 1000);
+        } else {
+            setTimeout(function () {
+                //Stop Game because we finished it
+                location.href = "./EndGame.html";
+                if (score > highScore) {
+                    localStorage.highscore = score;
+                    sessionStorage.bestScore = true;
+                    sessionStorage.score = score;
+                }
+            }, 8000);
+        }
 
-	}
+    }
 
 }
 
 function listenKeys() {
-	document.onkeydown = function (event) {
-		if (event.keyCode === 68) //d
-			player.pressingRight = true;
-		else if (event.keyCode === 83) //s
-			player.pressingDown = true;
-		else if (event.keyCode === 65) //a
-			player.pressingLeft = true;
-		else if (event.keyCode === 87) // w
-			player.pressingUp = true;
-		/* else if (event.keyCode === 32) // espace
-		     player.pressingSpace = true;*/
-	}
+    document.onkeydown = function (event) {
+        if (event.keyCode === 68) //d
+            player.pressingRight = true;
+        else if (event.keyCode === 83) //s
+            player.pressingDown = true;
+        else if (event.keyCode === 65) //a
+            player.pressingLeft = true;
+        else if (event.keyCode === 87) // w
+            player.pressingUp = true;
 
-	document.onkeyup = function (event) {
-		if (event.keyCode === 68) //d
-			player.pressingRight = false;
-		else if (event.keyCode === 83) //s
-			player.pressingDown = false;
-		else if (event.keyCode === 65) //a
-			player.pressingLeft = false;
-		else if (event.keyCode === 87) // w
-			player.pressingUp = false;
-		else if (event.keyCode === 80) // p for pause
-			pause = !pause;
-	}
+    }
+
+    document.onkeyup = function (event) {
+        if (event.keyCode === 68) //d
+            player.pressingRight = false;
+        else if (event.keyCode === 83) //s
+            player.pressingDown = false;
+        else if (event.keyCode === 65) //a
+            player.pressingLeft = false;
+        else if (event.keyCode === 87) // w
+            player.pressingUp = false;
+
+    }
 }
 
 
@@ -136,88 +136,79 @@ function listenKeys() {
 
 update = function () {
 
-	if (pause === true) {
-		bg.stop();
-		ctx.font = '100px Rockwell';
-		ctx.fillText("Pause", 650, 300);
-		//pause le level aussi
-		return;
-	}
-	if (pause === false) {
-		moveBackground();
-	}
-
-	if (frameCount != 0 && frameCount % 150 === 0) {
-		score += 10;
-	}
 
 
-	ctx.clearRect(0, 0, WIDTH, HEIGHT);
-	ctx.font = '50px Rockwell';
-
-	frameCount++;
-
-	for (var key in apricotList) {
-		apricotList[key].update();
-	}
-	for (var key in cheeseList) {
-		cheeseList[key].update();
-
-	}
-	for (var key in wineList) {
-		wineList[key].update();
-
-	}
-	for (var key in papetList) {
-		papetList[key].update();
-
-	}
-	for (var key in barrelList) {
-		barrelList[key].update();
-		player.testCollision(barrelList[key]);
-		if (collide) {
-			//player.hp-=1;
-			delete barrelList[key];
-			//delete viesList[player.hp + 1];
-		}
-	}
-	for (var key in viesList) {
-		viesList[key].update();
-
-	}
-	/*
-		for (var key in strawList) {
-			strawList[key].update();
-
-			player.testCollision(strawList[key]);
-			if (collide) {
-				score += 10;
-				player.hp -= 1;
-				delete viesList[player.hp + 1];
-				delete strawList[key];
-
-			}
+    if (frameCount != 0 && frameCount % 150 === 0) {
+        score += 10;
+    }
 
 
-		}
-	*/
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    ctx.font = '50px Rockwell';
 
-	/*
-	    for (var key in bonusList) {
-	        bonusList[key].update();
-	        var isColliding = player.testCollision(bonusList[key]);
-	        if (isColliding) {
-	            if (bonusList[key].category == 'abricot')
-	                score += 10;
-	            if (bonusList[key].category == 'wine')
-	                score += 25;
-	            if (bonusList[key].category == 'cheese')
-	                score += 50;
-	            delete bonusList[key];
-	        }
-	    }*/
+    frameCount++;
 
-	/*   for (var key in malusList) {
+    for (var key in apricotList) {
+        apricotList[key].update();
+    }
+    for (var key in cheeseList) {
+        cheeseList[key].update();
+
+    }
+    for (var key in wineList) {
+        wineList[key].update();
+
+    }
+    for (var key in papetList) {
+        papetList[key].update();
+
+    }
+    for (var key in barrelList) {
+        barrelList[key].update();
+        /*player.testCollision(barrelList[key]);
+        if (collide) {
+            player.hp -= 1;
+            delete barrelList[key];
+            delete viesList[player.hp + 1];
+        }*/
+    }
+    for (var key in viesList) {
+        viesList[key].update();
+
+    }
+    /*
+    	for (var key in strawList) {
+    		strawList[key].update();
+
+    		player.testCollision(strawList[key]);
+    		if (collide) {
+    			score += 10;
+    			player.hp -= 1;
+    			delete viesList[player.hp + 1];
+    			delete strawList[key];
+
+    		}
+
+
+    	}
+    */
+
+    /*
+        for (var key in bonusList) {
+            bonusList[key].update();
+            var isColliding = player.testCollision(bonusList[key]);
+            if (isColliding) {
+                if (bonusList[key].category == 'abricot')
+                    score += 10;
+                if (bonusList[key].category == 'wine')
+                    score += 25;
+                if (bonusList[key].category == 'cheese')
+                    score += 50;
+                delete bonusList[key];
+            }
+        }*/
+
+    /*   for (var key in malusList) {
         malusList[key].update();
         var isColliding = player.testCollision(malusList[key]);
         if (isColliding) {
@@ -230,56 +221,53 @@ update = function () {
         }
     }
 */
-	player.update();
+    player.update();
 
-	ctx.fillText('Score : ' + score, 1200, 655);
+    ctx.fillText('Score : ' + score, 1200, 655);
 	ctx.fillText('Best score : ' + highScore, 640, 655);
 
 
 }
 //Background
 moveBackground = function () {
-	$(function () {
-		deplace = function () {
+    $(function () {
+        deplace = function () {
 
-			bg = $('#fond').animate({
-				left: '-=250'
-			}, 1400, 'linear', function () {
-				$('#fond').css('top', 0);
-				deplace();
-			});
-		};
-		deplace();
-	});
-}
-
-drawMap = function () {
-
-	ctx.drawImage(Img.bg, 0, 0);
-
+            bg = $('#fond').animate({
+                left: '-=250'
+            }, 1400, 'linear', function () {
+                $('#fond').css('top', 0);
+                deplace();
+            });
+        };
+        deplace();
+    });
 }
 
 //Start game
 startGame = function () {
-	if (sessionStorage.isVaudois === "true") {
-		console.log(sessionStorage.isVaudois);
-		return generateGameOver();
-	}
-	player = Player();
-	drawMap();
-	listenKeys();
-	moveBackground();
-	generateVie();
-	generateLevel();
-	setInterval(update, 5);
+    if (sessionStorage.isVaudois === "true") {
+        console.log(sessionStorage.isVaudois);
+        return generateGameOver();
+    }
+    player = Player();
+    listenKeys();
+    moveBackground();
+    generateVie();
+    setInterval(update, 5);
 
-	//get the highscore from localstorage
-	var scoreStr = localStorage.highscore;
-	if (scoreStr == null) {
-		highScore = 0;
-	} else {
-		highScore = parseInt(scoreStr);
-	}
+    //get the highscore from localstorage
+    var scoreStr = localStorage.highscore;
+    if (scoreStr == null) {
+        highScore = 0;
+    } else {
+        highScore = parseInt(scoreStr);
+    }
+    setTimeout(function () {
+        generateLevel();
+    }, 2000);
+
+
 
 }
 
